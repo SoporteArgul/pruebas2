424f3759325c0a151b81a9750a50e146061f6515
mateo lonzayes <soporte@argul.com.ar>
2022-04-12 15:05:19 -0300

    avatar profile

--- indice/forms.py
+++ indice/forms.py
@@ -15,13 +15,11 @@ class UserRegisterForm(UserCreationForm):
         fields=['username','first_name','last_name','email','password1','password2']
         help_texts={k:'' for k in fields}
 
-class UserEditForm(UserCreationForm):
+class UserEditForm(forms.Form):
+  username=forms.CharField(label='username')
   email=forms.EmailField()
   password1=forms.CharField(label='Contraseña',widget=forms.PasswordInput)
   password2=forms.CharField(label='Repetir contraseña',widget=forms.PasswordInput)
   first_name=forms.CharField(label='Nombre',max_length=20,required=False)
   last_name=forms.CharField(label='Apellido',max_length=20)
-  class Meta:
-        model=User
-        fields=['username','email','password1','password2','first_name','last_name']
-        help_texts={k:'' for k in fields}
+  
--- indice/views.py
+++ indice/views.py
@@ -7,9 +7,10 @@ from django.contrib.auth import login ,authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .forms import UserRegisterForm, UserEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Avatar
 
 def inicio(request):
-    return render(request,'indice/index.html',{})
+    return render(request,'indice/index.html',{'user_avatar_url':buscar_url_avatar(request.user),})
 
 def otra_vista(request):
     return HttpResponse('''
@@ -53,7 +54,7 @@ def login_request(request):
            user=authenticate(username=username,password=password)
            if user is not None:
                login(request,user)
-               return render(request,'indice/index.html',{'msg':f'Te logueaste {username}!!'})
+               return render(request,'indice/index.html',{'msg':f'Te logueaste {username}!!','user_avatar_url':buscar_url_avatar(request.user)})
            else:
                return render(request,'indice/login.html',{'form':form, 'msg': 'Error vuelva a intentarlo'})
        else:
@@ -61,7 +62,6 @@ def login_request(request):
     else:
         form=AuthenticationForm()   
         return render(request,'indice/login.html',{'form':form,'msg':''})
-
 
 def register(request):    
     if request.method=='POST':
@@ -76,23 +76,39 @@ def register(request):
         form=UserRegisterForm()
     return render(request,'indice/register.html',{'form':form})
     
-
-
 @login_required
-def edit_profile(request): 
+def edit_profile(request):
+    msg=''
     usuario=request.user
     if request.method == 'POST':
         miFormulario=UserEditForm(request.POST)
         if miFormulario.is_valid():
             informacion=miFormulario.cleaned_data
-          
-            usuario.email=informacion['email']
-            usuario.password1=informacion.get('password1')        
-            usuario.password2=informacion.get('password2')
+            
+            usuario.email=informacion.get('email','')
+            #usuario.password1=informacion.get('password1')        
+            #usuario.password2=informacion.get('password2')
             usuario.first_name=informacion.get('first_name','')
             usuario.last_name=informacion.get('last_name','')
-            request.usuario.save()
-            return render(request,'indice/index.html',{})
-    else:
-        miFormulario=UserEditForm(initial={'email':usuario.email})
-    return render(request,'indice/editprofile.html',{'miFormulario':miFormulario,'usuario':usuario})
+            if informacion.get('password1')== informacion.get('password2') and len(informacion.get('password1'))>8:
+                usuario.set_password(informacion.get('password1'))
+           
+            else:
+                msg='no se modifico el password'
+            usuario.save()
+            return render(request,'indice/index.html',{'msg':msg,'user_avatar_url':buscar_url_avatar(request.user)})
+        else:
+            return render(request,'indice/editprofile.html',{'miFormulario':miFormulario,'msg':'','user_avatar_url':buscar_url_avatar(request.user)})
+    miFormulario=UserEditForm(initial={
+        'first_name':request.user.first_name,
+        'last_name':request.user.last_name,
+        'email':request.user.email,
+        'username':request.user.username})
+    return render(request, 'indice/editprofile.html',{'miFormulario':miFormulario,'msg':'','user_avatar_url':buscar_url_avatar(request.user)})
+    
+    
+
+def buscar_url_avatar(user):
+    return (Avatar.objects.filter(user=user)[0].avatar.url)
+
+#'user_avatar_url':buscar_url_avatar(request.user)
